08bf7ebc6f61ef493d79b158a0cc632f5e79decf
balford <balfordm21@gmail.com>
2020-01-17 13:57:58 -0600

    Parte 2

--- POO/poo_1.py
+++ POO/poo_1.py
@@ -6,35 +6,32 @@ class Carro():
     enmarcha = False
 
     #METODO
-    def arrancar(self):
+    def arrancar(self, arrancamos):
         
 #        pass#Hace que pase ya que no tiene parametros el metodo
-        self.enmarcha = True
-    
-    def estado(self):
+        self.enmarcha = arrancamos
+
         if(self.enmarcha):
             return "El carro esta en marcha"
         
         else:
-            return "El carro esta detenido" #Si no se llama al metido arrancar devolvera esto EJ: linea 28
+            return "El carro esta detenido"
+
+    def estado(self):
+        print("El carro tiene ", self.ruedas, " ruedas. Un ancho de ", self.anchoChasis, " y un largo de ", self.largoChasis)
 
 
 miCarro = Carro() ####Instanciando una clase
 
-print("El largo del coche es: ", miCarro.largoChasis)
-
-print("El carro tiene ", miCarro.ruedas, " ruedas")
-
-miCarro.arrancar()
-
-print(miCarro.estado())
+print(miCarro.arrancar(True))
+miCarro.estado()
 
 
 
 print("-----------------------A continuacion el segundo objeto----------------------")
 
 miCarro2 = Carro()
-print("El largo del coche es: ", miCarro.largoChasis)
-print("El carro tiene ", miCarro2.ruedas, " ruedas")
-print(miCarro2.estado())
+
+print(miCarro2.arrancar(False))
+miCarro2.estado()
 
