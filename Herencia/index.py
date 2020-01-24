fc426b5dcf4fb1b4451dd4e3a7fd90b19262547a
balford <balfordm21@gmail.com>
2020-01-23 20:00:12 -0600

    Herencia Parte 2

--- Herencia/index.py
+++ Herencia/index.py
@@ -1,5 +1,6 @@
 class Vehiculo():
-    def __init__(self, marca, modelo):
+
+    def __init__(self, marca, modelo):#Constructor
 
         self.marca = marca
         self.modelo = modelo
@@ -25,8 +26,64 @@ class Vehiculo():
 
 
 class Moto(Vehiculo): #Hereda de la SuperClase Vehiculos
-    pass
+
+    hcaballito = ""
+
+    def caballito(self):
+
+        self.hcaballito = "Voy haciendo el caballito"
+
+    def estado(self):
+
+        print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn Marcha: ", self.enmarcha, "\nacelera: ", self.acelera, "\nFrena: ", self.frena, "\n", self.hcaballito)    
+            #Se sobre escribe este metodo ya que estamos llamando a la clase hijo
+
+
+class Furgoneta(Vehiculo):
+
+    def cargar(self, cargar):
+
+        self.cargado = cargar
+
+        if(self.cargado):#Es igual a True
+
+            return "La furgoneta esta cargada"
+
+        else:
+
+            return "La furgoneta no esta cargada"
+        
+
 
 miMoto = Moto("Genesis", "RKS")
 
 miMoto.estado()
+miMoto.caballito()
+
+miFurgoneta = Furgoneta("Kia", "Picanto")
+
+miFurgoneta.estado()
+miFurgoneta.arrancar()
+print(miFurgoneta.cargar(True))
+
+
+
+class VElectricos():
+
+    def __init__(self):
+
+        self.autonomia = 100
+
+    def cargareEnergia(self):
+
+        self.cargando = True
+
+
+
+class BiciElectrica(Vehiculo, VElectricos):#Toma como prioridad los valores del constructor de la primera clase mencionada
+    pass
+
+
+miBici = BiciElectrica("HZ", "BMX") 
+
+
