7d14dfa707cab62d7a17696e6edca89bf1ea8d78
balford <balfordm21@gmail.com>
2020-01-28 20:02:41 -0600

    Archivos Faltantes

---
+++ MetodosCadenas/index.py
@@ -0,0 +1,24 @@
+'''
+upper = metodo para hacer mayuscula todas las letras de un string
+
+lower = pasa a mayuscula los string
+
+capitalize = pone la primera letra de un string en mayuscula
+
+count = contar las veces que aparecen una letra o una cadena en un string
+
+find = representa la posicion en la que aparece un caracter o un grupo de caracteres
+
+isdigit = devuelve un boolean donde dice si es un digito o no
+
+isalpha =  comprueba si hay solo letras (especios no cuentan)
+
+split = separa por palabras utilzando espacios
+
+strip = borra los espacios sobrantes del inicio y final
+
+replace = cambia una letra o palabra por otra
+
+rfind = representa el indice de una letra, contando desde el principio del string
+
+'''
